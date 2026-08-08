eecf9b89219ffb555d43aca05243d273edfd9921
cbrbr1986 <78068584+cbrbr1986@users.noreply.github.com>
2026-08-08 20:46:30 +0300

    Update build.gradle.kts

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -1,24 +1,3 @@
-plugins { id("com.android.application") }
-
-android {
-    namespace = "com.coskun.pdkspersonel"
-    compileSdk = 35
-    defaultConfig {
-        applicationId = "com.coskun.pdkspersonel"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
-    }
-}
-
-configurations.configureEach {
-    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
-    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
-    resolutionStrategy.force("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
-}
-
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+plugins {
+    id("com.android.application") version "8.7.3" apply false
 }
