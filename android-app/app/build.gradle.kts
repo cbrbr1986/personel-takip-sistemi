9f3b3432f1dcc92e9baddef0f9b85d83d35d9ad6
cbrbr1986 <78068584+cbrbr1986@users.noreply.github.com>
2026-08-08 21:06:50 +0300

    Add files via upload

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -6,6 +6,16 @@ plugins {
 android {
     namespace = "com.coskun.pdkspersonel"
     compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "com.coskun.pdkspersonel"
         minSdk = 26
