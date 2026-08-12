c3ed29ba1cb9eba351938b41258a26590eded5ff
cbrbr1986 <78068584+cbrbr1986@users.noreply.github.com>
2026-08-12 21:53:26 +0300

    Add files via upload

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.coskun.pdkspersonel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.3.2"
+        versionCode = 7
+        versionName = "1.3.3"
     }
 }
 
